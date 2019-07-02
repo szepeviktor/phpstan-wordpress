b1031a5da58bcc2c39d3d675f3de1778c8149e02
Viktor Szépe <viktor@szepe.net>
2019-07-02 15:22:14 +0000

    Example for detecting PHP warnings

---
+++ example/fix-php-errors.php
@@ -0,0 +1,9 @@
+<?php
+
+// Add this to your tests/phpstan/bootstrap.php file.
+function phpstan_error_handler( $errno, $errstr, $errfile, $errline ) {
+    if ( E_NOTICE !== $errno ) {
+        throw new \ErrorException( $errstr, 0, $errno, $errfile, $errline );
+    }
+}
+set_error_handler( 'phpstan_error_handler' );
