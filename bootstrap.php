f810cac9095aa45e2c8142a2e5e0d0a2572e656c
Viktor Szépe <viktor@szepe.net>
2019-08-03 22:39:03 +0000

    Support WP_DEBUG_LOG

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,7 @@
 // Core constants
 define( 'ABSPATH', '' );
 define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
 define( 'MINUTE_IN_SECONDS', 60 );
 define( 'HOUR_IN_SECONDS',   60 * MINUTE_IN_SECONDS );
 define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS );
