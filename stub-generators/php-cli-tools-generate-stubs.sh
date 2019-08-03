25c33cc2cef735a6cb0d287714d9bfe916a9be49
Viktor Szépe <viktor@szepe.net>
2019-08-03 23:27:37 +0000

    Separate WP-CLI/php-cli-tools stubs generator

--- stub-generators/php-cli-tools-generate-stubs.sh
+++ stub-generators/php-cli-tools-generate-stubs.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 #
-# Generate WP-CLI stubs.
+# Generate WP-CLI/php-cli-tools stubs.
 #
 
-PLUGIN_VERSION="2.2.0"
+PLUGIN_VERSION="0.11.11"
 
 GENERATE_STUBS_COMMAND="vendor/bin/generate-stubs"
 
 # Check plugin
-if [ ! -r ./php/wp-cli.php ]; then
-    echo "Please extract WP-CLI into the current directory!" 1>&2
-    echo "git clone https://github.com/wp-cli/wp-cli.git" 1>&2
+if [ ! -r ./lib/cli/cli.php ]; then
+    echo "Please clone php-cli-tools into the current directory!" 1>&2
+    echo "git clone https://github.com/wp-cli/php-cli-tools.git" 1>&2
     exit 10
 fi
-
-rm -v php/WP_CLI/ComposerIO.php
-rm -v php/WP_CLI/PackageManagerEventSubscriber.php
 
 # Generate stubs
 if hash generate-stubs 2>/dev/null; then
@@ -23,9 +20,8 @@ if hash generate-stubs 2>/dev/null; then
 elif hash generate-stubs.phar 2>/dev/null; then
     GENERATE_STUBS_COMMAND="generate-stubs.phar"
 elif [ ! -x vendor/bin/generate-stubs ]; then
-    rm composer.json composer.lock
     composer require --no-interaction --update-no-dev --prefer-dist --ignore-platform-reqs \
         giacocorsiglia/stubs-generator
 fi
-"$GENERATE_STUBS_COMMAND" --functions --classes --interfaces --traits --out=wp-cli-stubs-${PLUGIN_VERSION}.php \
-    ./php/
+"$GENERATE_STUBS_COMMAND" --functions --classes --out=php-cli-tools-stubs-${PLUGIN_VERSION}.php \
+    ./lib/
