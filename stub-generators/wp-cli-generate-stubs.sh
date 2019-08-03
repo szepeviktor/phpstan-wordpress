9ab76ff039c851a1b4015425dddfa64db69c28d1
Viktor Szépe <viktor@szepe.net>
2019-08-03 22:38:27 +0000

    WP-CLI stubs generation is hard

--- stub-generators/wp-cli-generate-stubs.sh
+++ stub-generators/wp-cli-generate-stubs.sh
@@ -5,6 +5,8 @@
 
 PLUGIN_VERSION="2.2.0"
 
+GENERATE_STUBS_COMMAND="vendor/bin/generate-stubs"
+
 # Check plugin
 if [ ! -r php/wp-cli.php ]; then
     echo "Please extract WP-CLI into the current directory!" 1>&2
@@ -12,13 +14,19 @@ if [ ! -r php/wp-cli.php ]; then
     exit 10
 fi
 
-rm composer.json composer.lock
 rm -v php/WP_CLI/ComposerIO.php
 rm -v php/WP_CLI/PackageManagerEventSubscriber.php
 
 # Generate stubs
-if [ ! -x vendor/bin/generate-stubs ]; then
-    composer require --no-interaction --update-no-dev --prefer-dist giacocorsiglia/stubs-generator
+if hash generate-stubs 2>/dev/null; then
+    GENERATE_STUBS_COMMAND="generate-stubs"
+elif hash generate-stubs.phar 2>/dev/null; then
+    GENERATE_STUBS_COMMAND="generate-stubs.phar"
+elif [ ! -x vendor/bin/generate-stubs ]; then
+    rm composer.json composer.lock
+    composer require --no-interaction --update-no-dev --prefer-dist --ignore-platform-reqs \
+        giacocorsiglia/stubs-generator
 fi
-vendor/bin/generate-stubs --functions --classes --interfaces --traits --out=wp-cli-stubs-${PLUGIN_VERSION}.php \
+# If WP-CLI is a dependency remove "./vendor/wp-cli/php-cli-tools/lib/" path
+"$GENERATE_STUBS_COMMAND" --functions --classes --interfaces --traits --out=wp-cli-stubs-${PLUGIN_VERSION}.php \
     ./php/ ./vendor/wp-cli/php-cli-tools/lib/
