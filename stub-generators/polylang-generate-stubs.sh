4189c72f457414fa11b218d4c94f3184c2e78cd9
Viktor Szépe <viktor@szepe.net>
2019-08-04 17:44:34 +0000

    Fix Polylang stubs generator

--- stub-generators/polylang-generate-stubs.sh
+++ stub-generators/polylang-generate-stubs.sh
@@ -3,7 +3,7 @@
 # Generate Polylang stubs.
 #
 
-PLUGIN_VERSION="2.5.3"
+PLUGIN_VERSION="2.6.2"
 
 # Check plugin
 if ! grep -q 'Plugin Name:\s\+Polylang' ./polylang.php 2>/dev/null; then
@@ -18,5 +18,5 @@ rm -v include/functions.php
 if [ ! -x vendor/bin/generate-stubs ]; then
     composer require --no-interaction --update-no-dev --prefer-dist giacocorsiglia/stubs-generator
 fi
-vendor/bin/generate-stubs --functions --classes --interfaces --traits --out=woocommerce-stubs-${PLUGIN_VERSION}.php \
+vendor/bin/generate-stubs --functions --classes --interfaces --traits --out=polylang-stubs-${PLUGIN_VERSION}.php \
     ./admin/ ./include/ ./lingotek/ ./settings/
