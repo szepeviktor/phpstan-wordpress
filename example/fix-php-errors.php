c3de4e200c3cd2ee0900211d66d7520794fe2ac4
Viktor Szépe <viktor@szepe.net>
2019-09-14 23:52:49 +0000

    Fix error handler

--- example/fix-php-errors.php
+++ example/fix-php-errors.php
@@ -5,5 +5,6 @@ function phpstan_error_handler( $errno, $errstr, $errfile, $errline ) {
     if ( E_NOTICE !== $errno && strpos($errstr, '/tmp/phpstan/') === false ) {
         throw new \ErrorException( $errstr, 0, $errno, $errfile, $errline );
     }
+    return true;
 }
 set_error_handler( 'phpstan_error_handler' );
