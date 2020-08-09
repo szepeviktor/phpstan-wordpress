929c5e8a986a345bf134763af9d6c54afe1a3864
full <fullbl@mail.com>
2020-08-09 09:34:18 +0200

    Added some core constants.

--- bootstrap.php
+++ bootstrap.php
@@ -4,12 +4,40 @@ declare(strict_types=1);
 
 // phpcs:disable Squiz.PHP.DiscouragedFunctions,NeutronStandard.Constants.DisallowDefine
 
-// There are no core functions to read these constants.
+// Directoy constants.
 define('ABSPATH', './');
+define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
+define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins');
+define('WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins');
+
+// URL constants.
+define('WP_CONTENT_URL', 'http://localhost/wp-content');
+define('WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins');
+define('WPMU_PLUGIN_URL', WP_CONTENT_URL . '/mu-plugins');
+
+// Debug constants.
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
-define('WPMU_PLUGIN_DIR', './');
+define('WP_DEBUG_DISPLAY', true);
+define('WP_DEBUG_LOG', false);
+define('WP_CACHE', false);
+define('SCRIPT_DEBUG', false);
+
+// WP underground constants - random value picked
+define('WP_START_TIMESTAMP', microtime(true));
+define('WP_MEMORY_LIMIT', '64M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 define('EMPTY_TRASH_DAYS', 30 * 86400);
+define('MEDIA_TRASH', false);
+define('SHORTINIT', false);
+define('WP_FEATURE_BETTER_PASSWORDS', true);
+define('FORCE_SSL_ADMIN', true);
+define('AUTOSAVE_INTERVAL', MINUTE_IN_SECONDS);
+define('WP_POST_REVISIONS', true);
+define('WP_CRON_LOCK_TIMEOUT', MINUTE_IN_SECONDS);
+define('WP_DEFAULT_THEME', 'twentytwenty');
+define('TEMPLATEPATH', WP_CONTENT_DIR . '/themes/twentytwenty');
+define('STYLESHEETPATH', WP_CONTENT_DIR . '/themes/twentytwenty');
 
 // Constants for expressing human-readable intervals.
 define('MINUTE_IN_SECONDS', 60);
@@ -25,8 +53,23 @@ define('MB_IN_BYTES', 1024 * KB_IN_BYTES);
 define('GB_IN_BYTES', 1024 * MB_IN_BYTES);
 define('TB_IN_BYTES', 1024 * GB_IN_BYTES);
 
-// wpdb method parameters.
+// Wpdb method parameters.
 define('OBJECT', 'OBJECT');
 define('OBJECT_K', 'OBJECT_K');
 define('ARRAY_A', 'ARRAY_A');
 define('ARRAY_N', 'ARRAY_N');
+
+// Cookies constants.
+define('COOKIEHASH', md5('http://localhost'));
+define('USER_COOKIE', 'wordpressuser_'. COOKIEHASH);
+define('PASS_COOKIE', 'wordpresspass_'. COOKIEHASH);
+define('AUTH_COOKIE', 'wordpress_'. COOKIEHASH);
+define('SECURE_AUTH_COOKIE', 'wordpress_sec_'. COOKIEHASH);
+define('LOGGED_IN_COOKIE', 'wordpress_logged_in_'. COOKIEHASH);
+define('TEST_COOKIE', 'wordpress_test_cookie');
+define('COOKIEPATH', '/');
+define('SITECOOKIEPATH', '/');
+define('ADMIN_COOKIE_PATH', SITECOOKIEPATH . 'wp-admin');
+define('PLUGINS_COOKIE_PATH', preg_replace('|https?://[^/]+|i', '', WP_PLUGIN_URL));
+define('COOKIE_DOMAIN', false);
+define('RECOVERY_MODE_COOKIE', 'wordpress_rec_' . COOKIEHASH);
