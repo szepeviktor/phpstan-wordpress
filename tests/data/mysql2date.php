9cbc69696d04635ff7b0ffada98bd896b30ae309
John Blackbourn <john@humanmade.com>
2021-10-24 13:23:05 +0200

    More relatable test data.

--- tests/data/mysql2date.php
+++ tests/data/mysql2date.php
@@ -9,11 +9,12 @@ use function PHPStan\Testing\assertType;
 // Constant strings
 assertType('int|false', mysql2date('G'));
 assertType('int|false', mysql2date('U'));
-assertType('string|false', mysql2date('Hello'));
+assertType('string|false', mysql2date('Y-m-d H:i:s'));
 
 // Unknown types
 assertType('mixed', $_GET['foo']);
 assertType('int|string|false', mysql2date($_GET['foo']));
+assertType('int|string|false', mysql2date(get_option('date_format')));
 
 // Unsupported types
 assertType('int|string|false', mysql2date(new \stdClass));
