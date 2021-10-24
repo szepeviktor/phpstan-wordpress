a942caeee3048dba92c0c3cae730ca97166799d1
John Blackbourn <john@humanmade.com>
2021-10-24 13:37:48 +0200

    Formatting.

--- tests/testTest.php
+++ tests/testTest.php
@@ -9,32 +9,32 @@ use PHPStan\Testing\TypeInferenceTestCase;
 class DynamicReturnTypeExtensionTests extends TypeInferenceTestCase
 {
 
-	/**
-	 * @return iterable<mixed>
-	 */
-	public function dataFileAsserts(): iterable
-	{
-		// path to a file with actual asserts of expected types:
-		yield from $this->gatherAssertTypes(__DIR__ . '/data/mysql2date.php');
-	}
+    /**
+     * @return iterable<mixed>
+     */
+    public function dataFileAsserts(): iterable
+    {
+        // path to a file with actual asserts of expected types:
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/mysql2date.php');
+    }
 
-	/**
-	 * @dataProvider dataFileAsserts
-	 */
-	public function testFileAsserts(
-		string $assertType,
-		string $file,
-		...$args
-	): void
-	{
-		$this->assertFileAsserts($assertType, $file, ...$args);
-	}
+    /**
+     * @dataProvider dataFileAsserts
+     */
+    public function testFileAsserts(
+        string $assertType,
+        string $file,
+        ...$args
+    ): void
+    {
+        $this->assertFileAsserts($assertType, $file, ...$args);
+    }
 
-	public static function getAdditionalConfigFiles(): array
-	{
-		// path to your project's phpstan.neon, or extension.neon in case of custom extension packages
-		return [__DIR__ . '/../extension.neon'];
-	}
+    public static function getAdditionalConfigFiles(): array
+    {
+        // path to your project's phpstan.neon, or extension.neon in case of custom extension packages
+        return [__DIR__ . '/../extension.neon'];
+    }
 
 }
 
