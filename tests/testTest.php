961f5d24cb98605b80d0e02179fffe7efd824fad
John Blackbourn <john@humanmade.com>
2021-10-24 21:33:59 +0200

    Add tests for `get_post()`.

--- tests/testTest.php
+++ tests/testTest.php
@@ -17,6 +17,7 @@ class DynamicReturnTypeExtensionTests extends TypeInferenceTestCase
         // path to a file with actual asserts of expected types:
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_comment.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_object_taxonomies.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/get_post.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/mysql2date.php');
     }
 
