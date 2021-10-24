17b3fc4e8effaf8ad33c15f0da83c55205c932ba
John Blackbourn <john@humanmade.com>
2021-10-24 17:18:39 +0200

    Add a dynamic return type extension for `current_time()`.

---
+++ tests/data/current_time.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SzepeViktor\PHPStan\WordPress\Tests;
+
+use function PHPStan\Testing\assertType;
+
+// Integer types
+assertType('int', current_time('timestamp'));
+assertType('int', current_time('U'));
+
+// String types
+assertType('string', current_time('mysql'));
+assertType('string', current_time('Hello'));
+
+// Unknown types
+assertType('int|string', current_time($_GET['foo']));
+assertType('int|string', current_time(get_option('date_format')));
+
+// Unsupported types
+assertType('int|string', current_time(new \stdClass));
+assertType('int|string', current_time(false));
