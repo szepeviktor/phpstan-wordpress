9de9476685cea48be3a42b01fe78709fbd6dd907
John Blackbourn <john@humanmade.com>
2021-10-24 12:31:52 +0200

    Add tests for `mysql2date()`.

---
+++ tests/data/mysql2date.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SzepeViktor\PHPStan\WordPress\Tests;
+
+use function PHPStan\Testing\assertType;
+
+class MySQL2DateAssertions
+{
+
+	public function mysql2date(): void
+	{
+		assertType('int|false', mysql2date('G'));
+		assertType('int|false', mysql2date('U'));
+		assertType('string|false', mysql2date('Hello'));
+
+		assertType('mixed', $_GET['foo']);
+		assertType('int|string|false', mysql2date($_GET['foo']));
+	}
+
+}
