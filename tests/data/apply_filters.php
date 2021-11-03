a1851ee7de4f8cbb7d169f83d10f355e8c1dd4cb
John Blackbourn <john@humanmade.com>
2021-11-03 12:22:19 +0000

    Tests for constant values.

--- tests/data/apply_filters.php
+++ tests/data/apply_filters.php
@@ -20,6 +20,25 @@ $value = apply_filters('filter',$foo);
 assertType('string', $value);
 
 /**
+ * Single constant type.
+ *
+ * @param string $foo Hello, World.
+ */
+$value = apply_filters('filter','I am a string');
+assertType('string', $value);
+
+/**
+ * Single constant of a type that differs from the docblock.
+ *
+ * @TODO Need to decide whether this should result in a type of `string|int` or whether we can
+ * get PHPStan to trigger a warning in this situation.
+ *
+ * @param string $foo Hello, World.
+ */
+$value = apply_filters('filter',123);
+assertType('string', $value);
+
+/**
  * Union type.
  *
  * @param string|null $foo Hello, World.
