9bc591bdfd841bb823fe4029ad214761bf6c2948
John Blackbourn <john@humanmade.com>
2021-11-04 11:52:49 +0000

    Test for more complex filter usage.

--- tests/data/apply_filters.php
+++ tests/data/apply_filters.php
@@ -85,3 +85,24 @@ assertType('array', $value);
  */
 $value = return_value(apply_filters('filter',$foo));
 assertType('string', $value);
+
+/**
+ * Filters the maximum image size dimensions for the editor.
+ *
+ * @since 2.5.0
+ *
+ * @param int[]        $max_image_size {
+ *     An array of width and height values.
+ *
+ *     @type int $0 The maximum width in pixels.
+ *     @type int $1 The maximum height in pixels.
+ * }
+ * @param string|int[] $size     Requested image size. Can be any registered image size name, or
+ *                               an array of width and height values in pixels (in that order).
+ * @param string       $context  The context the image is being resized for.
+ *                               Possible values are 'display' (like in a theme)
+ *                               or 'edit' (like inserting into an editor).
+ */
+list($max_width, $max_height) = apply_filters('editor_max_image_size', [$max_width, $max_height], $size, $context);
+assertType('int', $max_width);
+assertType('int', $max_height);
