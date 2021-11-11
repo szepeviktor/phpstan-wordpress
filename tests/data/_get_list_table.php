7a94a54c070e8a2ef9c61a027454fa52397962a2
Viktor Szépe <viktor@szepe.net>
2021-11-11 13:06:53 +0100

    Test *ERROR*

--- tests/data/_get_list_table.php
+++ tests/data/_get_list_table.php
@@ -12,3 +12,18 @@ assertType('Unknown_Table|false', _get_list_table('Unknown_Table'));
 
 // Unknown class name
 assertType('\WP_List_Table|false', _get_list_table(_GET['foo']));
+
+/**
+ * Returns the passed value.
+ *
+ * @template T
+ * @param T $value Value.
+ * @return T Value.
+ */
+function return_value( $value ) {
+    return $value;
+}
+
+$value = return_value(123);
+// Let's see!!
+assertType('int', $value);
