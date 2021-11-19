d20209f4086b1f8486373e14e53cf84f3b29d603
John Blackbourn <john@humanmade.com>
2021-11-19 00:28:01 +0000

    Add tests for `wp_insert_category()`.

--- tests/data/wp_error.php
+++ tests/data/wp_error.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SzepeViktor\PHPStan\WordPress\Tests;
 
 use function PHPStan\Testing\assertType;
+use function wp_insert_category;
 use function wp_insert_link;
 
 /**
@@ -20,4 +21,19 @@ $value = wp_insert_link([],true);
 assertType('int|WP_Error', $value);
 
 $value = wp_insert_link([],$_GET['wp_error']);
+assertType('int|WP_Error', $value);
+
+/**
+ * wp_insert_category()
+ */
+$value = wp_insert_category([]);
+assertType('int', $value);
+
+$value = wp_insert_category([],false);
+assertType('int', $value);
+
+$value = wp_insert_category([],true);
+assertType('int|WP_Error', $value);
+
+$value = wp_insert_category([],$_GET['wp_error']);
 assertType('int|WP_Error', $value);
