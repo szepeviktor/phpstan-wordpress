5f7f5c679eed10bd1c6e87e34aa33a13015559ae
John Blackbourn <john@humanmade.com>
2021-11-19 00:35:26 +0000

    Add tests for `wp_set_comment_status()`.

--- tests/data/wp_error.php
+++ tests/data/wp_error.php
@@ -7,6 +7,7 @@ namespace SzepeViktor\PHPStan\WordPress\Tests;
 use function PHPStan\Testing\assertType;
 use function wp_insert_category;
 use function wp_insert_link;
+use function wp_set_comment_status;
 
 /**
  * wp_insert_link()
@@ -37,3 +38,18 @@ assertType('int|WP_Error', $value);
 
 $value = wp_insert_category([],$_GET['wp_error']);
 assertType('int|WP_Error', $value);
+
+/**
+ * wp_set_comment_status()
+ */
+$value = wp_set_comment_status(1,'spam');
+assertType('bool', $value);
+
+$value = wp_set_comment_status(1,'spam',false);
+assertType('bool', $value);
+
+$value = wp_set_comment_status(1,'spam',true);
+assertType('WP_Error|true', $value);
+
+$value = wp_set_comment_status(1,'spam',$_GET['wp_error']);
+assertType('bool|WP_Error', $value);
