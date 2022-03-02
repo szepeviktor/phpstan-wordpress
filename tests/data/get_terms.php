15b47c44cbc0c9c1e69f0f619dcf31008eff5fc6
John Blackbourn <john@humanmade.com>
2022-03-02 19:25:19 +0000

    Start adding tests for `get_terms()`.

---
+++ tests/data/get_terms.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SzepeViktor\PHPStan\WordPress\Tests;
+
+use function PHPStan\Testing\assertType;
+
+$fields = $_GET['fields'] ?? 'all';
+
+// Default argument values
+assertType('array<int, WP_Term>', get_terms());
+assertType('array<int, WP_Term>', get_terms([]));
+assertType('array<int, WP_Term>', get_terms(''));
