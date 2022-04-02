87e1b81b935f3d15eb6bfb63b6d3014463ec5a74
John Blackbourn <john@humanmade.com>
2022-04-02 20:09:24 +0100

    Introduce a dynamic return type extension for `has_filter()` and `has_action()`.

--- tests/DynamicReturnTypeExtensionTest.php
+++ tests/DynamicReturnTypeExtensionTest.php
@@ -19,6 +19,7 @@ class DynamicReturnTypeExtensionTest extends \PHPStan\Testing\TypeInferenceTestC
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_object_taxonomies.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_post.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_terms.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/has_filter.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/mysql2date.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/shortcode_atts.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/term_exists.php');
