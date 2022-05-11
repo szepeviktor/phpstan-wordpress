bec48af9db11ee349114c82f2c300898f1543718
Martin Herndl <martin@herndl.org>
2022-05-11 10:32:04 +0200

    Add comment and simplify stub loading
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- wordpress-stubs.php
+++ wordpress-stubs.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-foreach ([__DIR__ . '/../../php-stubs/wordpress-stubs/wordpress-stubs.php', __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php'] as $file) {
-    if (!file_exists($file)) {
-        continue;
-    }
+// Locates and loads WordPress core stubs.
 
-    require_once $file;
-    break;
+foreach ([dirname(__DIR__, 2) . '/php-stubs/wordpress-stubs/wordpress-stubs.php', __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php'] as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+        break;
+    }
 }
