5909d3d2ec6018d2228dece978d0ec0d89861e85
Viktor Szépe <viktor@szepe.net>
2022-11-09 20:36:47 +0000

    Add type checking for hook callbacks in `add_action` and `add_filter` (#107) (#121)
    * Setup the hook callback rule and test.
    
    * Add the wp-hooks library.
    
    * Start adding tests.
    
    * Preparing more tests.
    
    * If we don't have enough arguments, bail out and let PHPStan handle the error.
    
    * If the callback is not valid, bail out and let PHPStan handle the error:
    
    * Simplify this.
    
    * More valid test cases.
    
    * Update some test line numbers.
    
    * First pass at detecting mismatching accepted and expected argument counts.
    
    * Remove some accidental duplicates.
    
    * Fix some logic.
    
    * Let's split this up before it gets out of hand.
    
    * Reduce this down.
    
    * More tidying up.
    
    * We're going to be needing this in multiple methods soon.
    
    * Prep for callback return type checking, not yet functional.
    
    * Split action return type assertion into its own method.
    
    * Bring this message more inline with those used by PHPStan.
    
    * Add detection for filter callbacks with no return value.
    
    * Don't need this just yet.
    
    * Use early exit to reduce code nesting.
    
    * Remove unused imports.
    
    * Yoda comparisons are disallowed.
    
    * Coding standards.
    
    * Remove unused code.
    
    * Use early return to reduce code nesting.
    
    * Remove more unused code.
    
    * Renaming.
    
    * Use early return to reduce code nesting.
    
    * Tidying up.
    
    * Correct logic introduced during refactoring.
    
    * Exclude "maybe" callables.
    
    * More handling for parameter counts.
    
    * More handling for optional parameters in hook callbacks.
    
    * Make the tests more manageable.
    
    * Tests for more callback types.
    
    * Tidying up.
    
    * This isn't used.
    
    * More test cases.
    
    * Tests for variadic callbacks.
    
    * More specific handling for variadic callbacks.
    
    * More tests.
    
    * The hooks names are not relevant to the tests.
    
    * Remove an `else`.
    
    * I'm still not entirely sure why this works, but it does. Props @herndlm.
    
    * Another test for an action with a return value.
    
    * More variadic handling.
    
    * Turns out PHPStan handles typed and untyped functions differently.
    
    * Partly broken callbacks will trigger an error in PHPStan so we don't need to handle them.
    
    * Terminology.
    
    * Refactoring.
    
    * PHP 7.2 compatibility.
    
    * Reorder the processing so the return type is checked first.
    
    * More tests.
    
    Co-authored-by: Viktor Szépe <viktor@szepe.net>
    
    Co-authored-by: John Blackbourn <john@humanmade.com>

---
+++ tests/data/hook-callback.php
@@ -0,0 +1,261 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SzepeViktor\PHPStan\WordPress\Tests;
+
+use function add_filter;
+use function add_action;
+
+// phpcs:disable Squiz.NamingConventions.ValidFunctionName.NotCamelCaps,Squiz.NamingConventions.ValidVariableName.NotCamelCaps,Squiz.NamingConventions.ValidVariableName.MemberNotCamelCaps
+
+/**
+ * Incorrect usage:
+ */
+
+// Filter callback return statement is missing.
+add_filter('filter', function() {});
+
+// Filter callback return statement is missing.
+add_filter('filter', function() {});
+
+// Filter callback return statement is missing.
+add_filter('filter', function(array $classes) {});
+
+// Callback expects 1 parameter, $accepted_args is set to 0.
+add_filter('filter', function($value) {
+    return 123;
+}, 10, 0);
+
+// Callback expects 1 parameter, $accepted_args is set to 2.
+add_filter('filter', function($value) {
+    return 123;
+}, 10, 2);
+
+// Callback expects 0-1 parameters, $accepted_args is set to 2.
+add_filter('filter', function($value = null) {
+    return 123;
+}, 10, 2);
+
+// Callback expects 2 parameters, $accepted_args is set to 1.
+add_filter('filter', function($value1, $value2) {
+    return 123;
+});
+
+// Callback expects 2-4 parameter, $accepted_args is set to 1.
+add_filter('filter', function($value1, $value2, $value3 = null, $value4 = null) {
+    return 123;
+});
+
+// Callback expects 2-3 parameter, $accepted_args is set to 4.
+add_filter('filter', function($value1, $value2, $value3 = null) {
+    return 123;
+}, 10, 4);
+
+// Action callback returns true but should not return anything.
+add_action('action', function() {
+    return true;
+});
+
+// Action callback returns true but should not return anything.
+add_action('action', function($value) {
+    if ($value) {
+        return true;
+    }
+});
+
+// Filter callback return statement is missing.
+add_filter('filter', __NAMESPACE__ . '\\no_return_value_typed');
+
+// Action callback returns false but should not return anything.
+add_action('action', '__return_false');
+
+// Action callback returns int but should not return anything.
+add_action('action', new TestInvokableTyped(), 10, 2);
+
+// Callback expects at least 1 parameter, $accepted_args is set to 0.
+add_filter('filter', __NAMESPACE__ . '\\filter_variadic_typed', 10, 0);
+
+// Callback expects at least 1 parameter, $accepted_args is set to 0.
+add_filter('filter', function( $one, ...$two ) {
+    return 123;
+}, 10, 0);
+
+// Callback expects 0-3 parameters, $accepted_args is set to 4.
+add_filter('filter', function($one = null, $two = null, $three = null) {
+    return 123;
+}, 10, 4);
+
+// Action callback returns int but should not return anything.
+add_action('action', __NAMESPACE__ . '\\return_value_typed');
+
+// Callback expects 0 parameters, $accepted_args is set to 2.
+add_filter('filter', '__return_false', 10, 2);
+
+// Action callback returns false but should not return anything (with incorrect number of accepted args).
+add_action('action', '__return_false', 10, 2);
+
+// Filter callback return statement is missing.
+add_filter('filter', __NAMESPACE__ . '\\no_return_value_untyped');
+
+/**
+ * Incorrect usage that's handled by PHPStan:
+ *
+ * These are here to ensure the rule doesn't trigger unwanted errors.
+ */
+
+// Too few parameters:
+add_filter('filter');
+add_filter();
+
+// Invalid callback:
+add_filter('filter', 'i_do_not_exist');
+
+// Invalid callback:
+add_filter('filter', __NAMESPACE__ . '\\i_do_not_exist');
+
+// Invalid callback:
+add_filter('filter', [new TestClassTyped, 'i_do_not_exist']);
+
+// Invalid callback:
+add_filter('filter', new TestClassTyped);
+
+// Unknown callback:
+add_filter('filter', $_GET['callback']);
+
+// Valid callback but with invalid parameters:
+add_filter('filter', function() {
+    return 123;
+}, false);
+add_filter('filter', function() {
+    return 123;
+}, 10, false);
+
+// Filter callback return statement may be missing.
+add_filter('filter', function($class) {
+    if ($class) {
+        return [];
+    }
+});
+
+// Incorrect function return type:
+add_filter('filter', function(array $classes): array {
+    return 123;
+});
+
+// Callback function with no declared return type.
+add_action('action', __NAMESPACE__ . '\\return_value_untyped');
+
+/**
+ * Correct usage:
+ */
+
+add_filter('filter', function() {
+    return 123;
+}, 10, 0);
+add_filter('filter', function() {
+    // We're allowing 0 parameters when `$accepted_args` is default value of 1.
+    // This might change in the future to get more strict.
+    return 123;
+});
+add_filter('filter', function($value) {
+    return 123;
+});
+add_filter('filter', function($value) {
+    return 123;
+}, 10, 1);
+add_filter('filter', function($value1, $value2) {
+    return 123;
+}, 10, 2);
+add_filter('filter', function($value1, $value2, $value3 = null) {
+    return 123;
+}, 10, 2);
+add_filter('filter', function($value1, $value2, $value3 = null) {
+    return 123;
+}, 10, 3);
+add_filter('filter', function($value = null) {
+    return 123;
+});
+add_filter('filter', function($value = null) {
+    return 123;
+}, 10, 0);
+add_filter('filter', function($value = null) {
+    return 123;
+}, 10, 1);
+add_filter('filter', function($one = null, $two = null, $three = null) {
+    return 123;
+});
+
+// Action callbacks must return void
+add_action('action', function() {
+    return;
+});
+add_action('action', function() {
+});
+add_action('action', function(): void {
+});
+add_action('action', __NAMESPACE__ . '\\no_return_value_typed');
+
+// Filter callback may exit, unfortunately
+add_filter('filter', function(array $classes) {
+    if ($classes) {
+        exit('Goodbye');
+    }
+
+    return $classes;
+});
+
+// Action callback may exit, unfortunately
+add_action('action', function($result) {
+    if (! $result) {
+        exit('Goodbye');
+    }
+});
+
+// Various callback types
+add_filter('filter', '__return_false');
+add_filter('filter', __NAMESPACE__ . '\\return_value_typed');
+add_filter('filter', new TestInvokableTyped(), 10, 2);
+add_filter('filter', [new TestClassTyped, 'foo']);
+
+$test_class = new TestClassTyped();
+
+add_filter('filter', [$test_class, 'foo']);
+
+// Variadic callbacks
+add_filter('filter', __NAMESPACE__ . '\\filter_variadic_typed');
+add_filter('filter', __NAMESPACE__ . '\\filter_variadic_typed', 10, 1);
+add_filter('filter', __NAMESPACE__ . '\\filter_variadic_typed', 10, 2);
+add_filter('filter', __NAMESPACE__ . '\\filter_variadic_typed', 10, 999);
+
+/**
+ * Symbol definitions for use in these tests.
+ */
+
+function no_return_value_typed( $value ) : void {}
+
+function return_value_typed() : int {
+    return 123;
+}
+
+function no_return_value_untyped( $value ) {}
+
+function return_value_untyped() {
+    return 123;
+}
+
+function filter_variadic_typed( $one, ...$two ) : int {
+    return 123;
+}
+
+class TestInvokableTyped {
+    public function __invoke($one, $two) : int {
+        return 123;
+    }
+}
+
+class TestClassTyped {
+    public function foo() : int {
+        return 123;
+    }
+}
