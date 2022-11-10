64a78177e0646a8b089e6d1ec976e27b9f9b4529
Viktor Szépe <viktor@szepe.net>
2022-11-10 17:12:23 +0000

    Remove Foo

--- tests/data/assert_wp_error.php
+++ tests/data/assert_wp_error.php
@@ -7,23 +7,16 @@ namespace SzepeViktor\PHPStan\WordPress\Tests;
 use WP_UnitTestCase_Base;
 use function PHPStan\Testing\assertType;
 
-class Foo {
-    public function foo($foo) {
-
-    }
-};
-
 class AssertWpError
 {
 
     public function inheritedAssertMethodsNarrowType(): void
     {
         /** @var \WP_Error|int $yes */
-        $yes = $yes;
+        $yes = $_GET['yes'];
 
         $customAsserter = new class () extends WP_UnitTestCase_Base {};
         $customAsserter->assertWPError($yes);
-        (new Foo())->foo($yes);
         assertType('WP_Error', $yes);
     }
 
