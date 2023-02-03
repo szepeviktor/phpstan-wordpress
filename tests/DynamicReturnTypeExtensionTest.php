86a582fad0db949490dc1f7b379e2fe8a0d06319
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-02-04 00:44:00 +0100

    Add dynamic return type extension for get_permalink()

--- tests/DynamicReturnTypeExtensionTest.php
+++ tests/DynamicReturnTypeExtensionTest.php
@@ -19,6 +19,7 @@ class DynamicReturnTypeExtensionTest extends \PHPStan\Testing\TypeInferenceTestC
         yield from $this->gatherAssertTypes(__DIR__ . '/data/esc_sql.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_comment.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_object_taxonomies.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/get_permalink.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_post.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_posts.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_sites.php');
