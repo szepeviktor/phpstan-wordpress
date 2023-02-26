03133fcf2d8513197cf5ea359c116f8956b55ac0
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-02-26 02:38:19 +0100

    Fix return type extension for get_sites()

--- src/GetSitesDynamicFunctionReturnTypeExtension.php
+++ src/GetSitesDynamicFunctionReturnTypeExtension.php
@@ -15,8 +15,8 @@ use PHPStan\Type\Type;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\ObjectType;
-use PHPStan\Type\Constant\ConstantArrayType;
-use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\TypeCombinator;
+use WP_Site;
 
 class GetSitesDynamicFunctionReturnTypeExtension implements \PHPStan\Type\DynamicFunctionReturnTypeExtension
 {
@@ -36,41 +36,89 @@ class GetSitesDynamicFunctionReturnTypeExtension implements \PHPStan\Type\Dynami
 
         // Called without arguments
         if (count($args) === 0) {
-            return new ArrayType(new IntegerType(), new ObjectType('WP_Site'));
+            return new ArrayType(new IntegerType(), new ObjectType(WP_Site::class));
         }
-
-        $fields = '';
 
         $argumentType = $scope->getType($args[0]->value);
 
-        // Called with an array argument
-        if ($argumentType instanceof ConstantArrayType) {
-            foreach ($argumentType->getKeyTypes() as $index => $key) {
-                if (! $key instanceof ConstantStringType || $key->getValue() !== 'fields') {
-                    continue;
-                }
+        // Called with a non constant argument
+        if (
+            count($argumentType->getConstantArrays()) === 0 &&
+            count($argumentType->getConstantStrings()) === 0
+        ) {
+            return TypeCombinator::union(
+                new ArrayType(new IntegerType(), new ObjectType(WP_Site::class)),
+                new ArrayType(new IntegerType(), new IntegerType()),
+                new IntegerType()
+            );
+        }
 
-                $fieldsType = $argumentType->getValueTypes()[$index];
-                if ($fieldsType instanceof ConstantStringType) {
-                    $fields = $fieldsType->getValue();
+        $fields = [];
+        $count = [];
+        $returnType = [];
+
+        // Called with a constant array argument
+        if (count($argumentType->getConstantArrays()) !== 0) {
+            foreach ($argumentType->getConstantArrays() as $constantArray) {
+                foreach ($constantArray->getKeyTypes() as $index => $key) {
+                    if (count($key->getConstantStrings()) === 0) {
+                        continue;
+                    }
+                    foreach ($key->getConstantStrings() as $constantKey) {
+                        if (!in_array($constantKey->getValue(), ['fields', 'count'], true)) {
+                            continue;
+                        }
+                        $fieldsType = $constantArray->getValueTypes()[$index];
+                        if (count($fieldsType->getConstantScalarValues()) === 0) {
+                            continue;
+                        }
+
+                        foreach ($fieldsType->getConstantScalarTypes() as $constantField) {
+                            if ($constantKey->getValue() === 'fields') {
+                                $fields[] = $constantField->getValue();
+                            }
+                            if ($constantKey->getValue() !== 'count') {
+                                continue;
+                            }
+
+                            $count[] = (bool)$constantField->getValue();
+                        }
+                    }
                 }
-                break;
+            }
+            if ($fields === []) {
+                $fields = [''];
+            }
+            if ($count === []) {
+                $count = [false];
             }
         }
 
-        // Called with a string argument
-        if ($argumentType instanceof ConstantStringType) {
-            parse_str($argumentType->getValue(), $variables);
-            $fields = $variables['fields'] ?? 'all';
+        // Called with a constant string argument
+        if (count($argumentType->getConstantStrings()) !== 0) {
+            foreach ($argumentType->getConstantStrings() as $constantString) {
+                parse_str($constantString->getValue(), $variables);
+                $fields[] = $variables['fields'] ?? '';
+                $count[] = isset($variables['count']) ? (bool)$variables['count'] : false;
+            }
         }
 
-        switch ($fields) {
-            case 'count':
-                return new IntegerType();
-            case 'ids':
-                return new ArrayType(new IntegerType(), new IntegerType());
-            default:
-                return new ArrayType(new IntegerType(), new ObjectType('WP_Site'));
+        if (in_array(true, $count, true) && count($count) === 1) {
+            return new IntegerType();
         }
+
+        if (in_array(true, $count, true)) {
+            $returnType[] = new IntegerType();
+        }
+
+        if (in_array('ids', $fields, true)) {
+            $returnType[] = new ArrayType(new IntegerType(), new IntegerType());
+        }
+
+        if (!in_array('ids', $fields, true)) {
+            $returnType[] = new ArrayType(new IntegerType(), new ObjectType(WP_Site::class));
+        }
+
+        return TypeCombinator::union(...$returnType);
     }
 }
