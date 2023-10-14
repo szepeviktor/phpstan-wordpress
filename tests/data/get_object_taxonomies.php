be5bfbf3dfda6fa0ad3e1eb0b654f94017e2f18d
Viktor Szépe <viktor@szepe.net>
2023-10-14 20:48:49 +0200

    Update get_object_taxonomies.php

--- tests/data/get_object_taxonomies.php
+++ tests/data/get_object_taxonomies.php
@@ -12,7 +12,7 @@ assertType('array<int, string>', get_object_taxonomies('post', 'names'));
 assertType('array<int, string>', get_object_taxonomies('post', 'Hello'));
 
 // Unknown output
-assertType('array<string|WP_Taxonomy>', get_object_taxonomies('post', $_GET['foo']));
+assertType('array<int|string, string|WP_Taxonomy>', get_object_taxonomies('post', $_GET['foo']));
 
 // Objects output
 assertType('array<string, WP_Taxonomy>', get_object_taxonomies('post', 'objects'));
