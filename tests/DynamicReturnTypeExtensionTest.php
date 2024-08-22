51cc7450c0ddf391bacc492899ff95f9439c4e67
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-08-22 21:53:25 +0200

    remove get_approved_comments extension

--- tests/DynamicReturnTypeExtensionTest.php
+++ tests/DynamicReturnTypeExtensionTest.php
@@ -15,7 +15,6 @@ class DynamicReturnTypeExtensionTest extends \PHPStan\Testing\TypeInferenceTestC
         yield from self::gatherAssertTypes(__DIR__ . '/data/_get_list_table.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/apply_filters.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/echo_key.php');
-        yield from self::gatherAssertTypes(__DIR__ . '/data/get_approved_comments.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/esc_sql.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/get_post.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/get_sites.php');
