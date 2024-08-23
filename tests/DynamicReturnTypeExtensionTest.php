409475daf3c1847fc70778e2596430bee523f9cd
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-08-23 14:26:14 +0200

    remove get_terms extension

--- tests/DynamicReturnTypeExtensionTest.php
+++ tests/DynamicReturnTypeExtensionTest.php
@@ -17,7 +17,6 @@ class DynamicReturnTypeExtensionTest extends \PHPStan\Testing\TypeInferenceTestC
         yield from self::gatherAssertTypes(__DIR__ . '/data/echo_key.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/esc_sql.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/get_post.php');
-        yield from self::gatherAssertTypes(__DIR__ . '/data/get_terms.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/shortcode_atts.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/wp_die.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/wp_parse_url.php');
