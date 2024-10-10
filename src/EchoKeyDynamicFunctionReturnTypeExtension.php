e4e72dec3fe27813b2f86d0a3d5af236a471e639
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2024-10-10 08:16:38 +0200

    Remove return extension for wp_dropdown_pages (#247)
    As documented, `wp_dropdown_pages()` always returns a string. See lines 1245-1249 in [wp-includes/post-template.php](https://github.com/WordPress/WordPress/blob/e25ffd14269ad6461aba63e3119ac5603502bf06/wp-includes/post-template.php#L1245-L1249).

--- src/EchoKeyDynamicFunctionReturnTypeExtension.php
+++ src/EchoKeyDynamicFunctionReturnTypeExtension.php
@@ -30,7 +30,6 @@ class EchoKeyDynamicFunctionReturnTypeExtension implements \PHPStan\Type\Dynamic
         'get_search_form' => 0,
         'the_title_attribute' => 0,
         'wp_dropdown_languages' => 0,
-        'wp_dropdown_pages' => 0,
         'wp_dropdown_users' => 0,
         'wp_get_archives' => 0,
         'wp_link_pages' => 0,
@@ -89,7 +88,6 @@ class EchoKeyDynamicFunctionReturnTypeExtension implements \PHPStan\Type\Dynamic
      * @see https://developer.wordpress.org/reference/functions/get_search_form/
      * @see https://developer.wordpress.org/reference/functions/the_title_attribute/
      * @see https://developer.wordpress.org/reference/functions/wp_dropdown_languages/
-     * @see https://developer.wordpress.org/reference/functions/wp_dropdown_pages/
      * @see https://developer.wordpress.org/reference/functions/wp_dropdown_users/
      * @see https://developer.wordpress.org/reference/functions/wp_get_archives/
      * @see https://developer.wordpress.org/reference/functions/wp_link_pages/
