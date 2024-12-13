1cda060b53f41af29cbb0c7f8400fbf2f16e2f56
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-12-13 21:26:15 +0100

    Add rule to suggest replacing WP constants with function calls

---
+++ tests/data/wp-constants.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SzepeViktor\PHPStan\WordPress\Tests;
+
+// WpConstantFetchRule
+$ruleDoesNotApply = MB_IN_BYTES;
+$ruleApplies = BACKGROUND_COLOR;
+$ruleApplies = BACKGROUND_IMAGE;
+$ruleApplies = DISALLOW_FILE_MODS;
+$ruleApplies = DOING_AJAX;
+$ruleApplies = DOING_CRON;
+$ruleApplies = FORCE_SSL_ADMIN;
+$ruleApplies = FORCE_SSL_LOGIN;
+$ruleApplies = FS_METHOD;
+$ruleApplies = MULTISITE;
+$ruleApplies = STYLESHEETPATH;
+$ruleApplies = SUBDOMAIN_INSTALL;
+$ruleApplies = TEMPLATEPATH;
+$ruleApplies = UPLOADS;
+$ruleApplies = VHOST;
+$ruleApplies = WP_ADMIN;
+$ruleApplies = WP_BLOG_ADMIN;
+$ruleApplies = WP_CONTENT_URL;
+$ruleApplies = WP_DEVELOPMENT_MODE;
+$ruleApplies = WP_HOME;
+$ruleApplies = WP_INSTALLING;
+$ruleApplies = WP_NETWORK_ADMIN;
+$ruleApplies = WP_PLUGIN_URL;
+$ruleApplies = WP_POST_REVISIONS;
+$ruleApplies = WP_SITEURL;
+$ruleApplies = WP_USER_ADMIN;
+$ruleApplies = WP_USE_THEMES;
+$ruleApplies = WPMU_PLUGIN_URL;
