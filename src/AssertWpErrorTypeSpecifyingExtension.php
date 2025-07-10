bf741683658691aed5eec103a9d9e31c92de4774
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-07-11 01:06:11 +0200

    Merge Assert(Not)WpErrorTypeSpecifyingExtensions

--- src/AssertWpErrorTypeSpecifyingExtension.php
+++ src/AssertWpErrorTypeSpecifyingExtension.php
@@ -1,23 +1,29 @@
 <?php
 
 /**
- * Set specified type of WP_UnitTestCase_Base::assertWPError().
+ * Set specified type of WP_UnitTestCase_Base::assertWPError and
+ * WP_UnitTestCase_Base::assertNotWPError.
  */
 
 declare(strict_types=1);
 
 namespace SzepeViktor\PHPStan\WordPress;
 
+use PhpParser\Node\Expr\BooleanNot;
+use PhpParser\Node\Expr\Instanceof_;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Name;
 use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
 use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\Reflection\MethodReflection;
-use PHPStan\Type\ObjectType;
 
 class AssertWpErrorTypeSpecifyingExtension implements \PHPStan\Type\MethodTypeSpecifyingExtension, \PHPStan\Analyser\TypeSpecifierAwareExtension
 {
+    private const ASSERT = 'assertWPError';
+    private const ASSERT_NOT = 'assertNotWPError';
+
     private TypeSpecifier $typeSpecifier;
 
     public function getClass(): string
@@ -27,7 +33,7 @@ class AssertWpErrorTypeSpecifyingExtension implements \PHPStan\Type\MethodTypeSp
 
     public function isMethodSupported(MethodReflection $methodReflection, MethodCall $node, TypeSpecifierContext $context): bool
     {
-        return strtolower($methodReflection->getName()) === 'assertwperror'
+        return in_array($methodReflection->getName(), [self::ASSERT, self::ASSERT_NOT], true)
             && isset($node->args[0])
             && $context->null();
     }
@@ -35,13 +41,16 @@ class AssertWpErrorTypeSpecifyingExtension implements \PHPStan\Type\MethodTypeSp
     // phpcs:ignore SlevomatCodingStandard.Functions.UnusedParameter
     public function specifyTypes(MethodReflection $methodReflection, MethodCall $node, Scope $scope, TypeSpecifierContext $context): SpecifiedTypes
     {
-        $args = $node->getArgs();
+        $expression = new Instanceof_($node->getArgs()[0]->value, new Name('WP_Error'));
 
-        return $this->typeSpecifier->create(
-            $args[0]->value,
-            new ObjectType(\WP_Error::class),
+        if ($methodReflection->getName() === self::ASSERT_NOT) {
+            $expression = new BooleanNot($expression);
+        }
+
+        return $this->typeSpecifier->specifyTypesInCondition(
+            $scope,
+            $expression,
             TypeSpecifierContext::createTruthy(),
-            $scope
         );
     }
 
