e6c221977ad249c4f587a4eeaeea5dcbd00e8f5f
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-09-14 00:43:58 +0200

    Gather assert types conditional on installed PHPStan version

--- tests/DynamicReturnTypeExtensionTest.php
+++ tests/DynamicReturnTypeExtensionTest.php
@@ -11,16 +11,21 @@ class DynamicReturnTypeExtensionTest extends \PHPStan\Testing\TypeInferenceTestC
      */
     public function dataFileAsserts(): iterable
     {
-        // Path to a file with actual asserts of expected types:
         yield from self::gatherAssertTypes(__DIR__ . '/data/apply-filters.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/ApplyFiltersTestClass.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/esc-sql.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/normalize-whitespace.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/shortcode-atts.php');
-        yield from self::gatherAssertTypes(__DIR__ . '/data/slashit-functions.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/stripslashes-from-strings-only.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/wp-parse-url.php');
         yield from self::gatherAssertTypes(__DIR__ . '/data/wp-slash.php');
+
+        $phpstanVersion = self::getContainer()->getByType(InstalledPhpStanVersion::class);
+
+        if ($phpstanVersion->satisfies('^2.1.18')) {
+            // Improved rtrim handling in PHPStan 2.1.18 gives different results
+            yield from self::gatherAssertTypes(__DIR__ . '/data/slashit-functions.php');
+        }
     }
 
     /**
@@ -34,6 +39,9 @@ class DynamicReturnTypeExtensionTest extends \PHPStan\Testing\TypeInferenceTestC
 
     public static function getAdditionalConfigFiles(): array
     {
-        return [dirname(__DIR__) . '/vendor/szepeviktor/phpstan-wordpress/extension.neon'];
+        return [
+            dirname(__DIR__) . '/vendor/szepeviktor/phpstan-wordpress/extension.neon',
+            __DIR__ . '/test-services.neon',
+        ];
     }
 }
